6d8278bbf2e782d7c70f9e8ad10365a4e3089410
William Tremblay <tremwil@gmail.com>
2026-01-13 01:21:21 +0000

    clean up and add some docs

--- src/alphabet.rs
+++ src/alphabet.rs
@@ -1,5 +1,5 @@
 use std::{
-    ops::{BitAnd, BitOr, Range},
+    ops::Range,
     simd::{
         LaneCount, Mask, Simd, SupportedLaneCount,
         cmp::{SimdPartialEq, SimdPartialOrd},
@@ -23,6 +23,13 @@ const fn sort_bytes<const N: usize>(mut bytes: [u8; N]) -> [u8; N] {
     bytes
 }
 
+/// Compile-time preprocessed alphabet.
+///
+/// Stores the sorted bytes as well as the the contiguous ranges making up this alphabet.
+///
+/// Because this is all computed at compile-time, the optimizer can generate highly optimized code
+/// for [`Self::contains`], [`Self::simd_prefilter`] and unroll loops that iterate on
+/// [`Self::simd_groups`].
 #[derive(Debug, Clone)]
 pub struct Alphabet<const N: usize> {
     bytes: [u8; N],
@@ -52,48 +59,6 @@ impl<const N: usize> Alphabet<N> {
         }
     }
 
-    pub const fn bytes(&self) -> &[u8; N] {
-        &self.bytes
-    }
-
-    pub const fn ranges(&self) -> &[Range<u32>] {
-        self.ranges.as_slice()
-    }
-
-    pub const fn contains(&self, char: u32) -> bool {
-        if self.ranges.is_empty() {
-            return false;
-        }
-
-        let mut i = self.ranges.len() - 1;
-        loop {
-            if char >= self.ranges.index(i).end {
-                return false;
-            }
-            if char >= self.ranges.index(i).start {
-                return true;
-            }
-            if i == 0 {
-                return false;
-            }
-            i -= 1;
-        }
-    }
-
-    #[inline(always)]
-    pub fn simd_prefilter<const L: usize>(&self, chars: Simd<u32, L>) -> bool
-    where
-        LaneCount<L>: SupportedLaneCount,
-        Simd<u32, L>: SimdPartialEq<Mask = Mask<i32, L>>,
-    {
-        if self.ranges.is_empty() {
-            return false;
-        }
-
-        let alphabet_end = self.ranges[self.ranges.len() - 1].end;
-        chars.simd_lt(Simd::splat(alphabet_end)).any()
-    }
-
     const fn compute_ranges(sorted: &[u8; N]) -> ConstVec<Range<u32>, N> {
         const U8_SIZE: u32 = u8::MAX as u32 + 1;
 
@@ -118,7 +83,49 @@ impl<const N: usize> Alphabet<N> {
         ranges
     }
 
-    pub const fn simd_groups<const L: usize>(&self) -> (ConstVec<Simd<u32, L>, N>, ConstVec<u32, L>)
+    pub const fn bytes(&self) -> &[u8; N] {
+        &self.bytes
+    }
+
+    #[inline(always)]
+    pub const fn contains(&self, char: u32) -> bool {
+        if self.ranges.is_empty() {
+            return false;
+        }
+
+        let mut i = self.ranges.len() - 1;
+        loop {
+            if char >= self.ranges.index(i).end {
+                return false;
+            }
+            if char >= self.ranges.index(i).start {
+                return true;
+            }
+            if i == 0 {
+                return false;
+            }
+            i -= 1;
+        }
+    }
+
+    /// Quickly eliminate vectors for which none of the elements are in this alphabet.
+    #[inline(always)]
+    pub fn simd_prefilter<const L: usize>(&self, chars: Simd<u32, L>) -> bool
+    where
+        LaneCount<L>: SupportedLaneCount,
+        Simd<u32, L>: SimdPartialEq<Mask = Mask<i32, L>>,
+    {
+        if self.ranges.is_empty() {
+            return false;
+        }
+
+        let alphabet_end = self.ranges[self.ranges.len() - 1].end;
+        chars.simd_lt(Simd::splat(alphabet_end)).any()
+    }
+
+    /// Split the characters of the alphabet into `floor(N/L)` SIMD vectors and a remainder
+    /// array of less `N % L` elements.
+    pub const fn simd_chunks<const L: usize>(&self) -> (ConstVec<Simd<u32, L>, N>, ConstVec<u32, L>)
     where
         LaneCount<L>: SupportedLaneCount,
     {
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
 #![feature(portable_simd)]
 #![feature(likely_unlikely)]
 
-use rayon::prelude::*;
 use std::{
     hint::unlikely,
-    simd::{cmp::SimdPartialEq, *},
+    simd::{LaneCount, Mask, Simd, SupportedLaneCount, cmp::SimdPartialEq},
     time::Instant,
 };
 
 mod alphabet;
 mod const_vec;
-
-// /other/m_q4ww2d.dcx
 
 use alphabet::Alphabet;
 
@@ -19,15 +16,21 @@ const PREFIX: &[u8] = b"/other/";
 const SUFFIX: &[u8] = b".dcx";
 
 const ALPHABET: Alphabet<38> = Alphabet::new(b"_.abcdefghijklmnopqrstuvwxyz0123456789");
-//const ALPHABET: &[u8] = b"._abcdefghijklmnopqrstuvwxyz0123456789";
 
 const START: &[u8] = b"mnopqrs";
 const TARGET: u32 = 0xd7255946;
 const SEARCH: usize = 7;
 
+/// Note that this isn't the real FNV prime, but what FromSoft uses.
 const FNV_PRIME: u32 = 37;
 
+/// Precomputed information about the hash of a suffix.
+///
+/// Used to efficiently compute the combined hash of `base|suffix` given `hash(base)`
+/// as well as efficiently finding a single character `x` such that
+/// `hash(base|x|suffix) == target_hash`.
 #[derive(Debug, Clone, Copy)]
+#[allow(unused)]
 struct PrecomputedSuffix {
     hash: u32,
     mult: u32,
@@ -63,32 +66,6 @@ impl PrecomputedSuffix {
     }
 }
 
-const fn fnv_hash(data: &[u8]) -> u32 {
-    let mut hash: u32 = 0;
-    let mut i = 0;
-    while i < data.len() {
-        hash = hash.wrapping_mul(FNV_PRIME).wrapping_add(data[i] as u32);
-        i += 1;
-    }
-    hash
-}
-
-fn main() {
-    let now = Instant::now();
-
-    for m in find_collisions_simd::<4>(b"/other/m", SUFFIX, SEARCH, TARGET) {
-        let slice = &m.bytes()[..m.len];
-        println!(
-            "/other/m{}{}",
-            String::from_utf8_lossy(slice),
-            String::from_utf8_lossy(SUFFIX)
-        )
-    }
-
-    //blast_all();
-    println!("{:?}", now.elapsed());
-}
-
 #[derive(Debug, Clone, Copy)]
 struct Match {
     bytes_be: u64,
@@ -103,6 +80,54 @@ impl Match {
     }
 }
 
+fn main() {
+    let now = Instant::now();
+
+    let mut prefix = PREFIX.to_owned();
+    prefix.push(0);
+
+    for &start_char in START {
+        *prefix.last_mut().unwrap() = start_char;
+
+        for m in find_collisions_simd::<4>(&prefix, SUFFIX, SEARCH, TARGET) {
+            let match_bytes = &m.bytes()[..m.len];
+
+            let mut collision = prefix.clone();
+            collision.extend_from_slice(match_bytes);
+            collision.extend_from_slice(SUFFIX);
+
+            println!("{}", String::from_utf8_lossy(&collision));
+
+            // for validation purposes
+            assert_eq!(fnv_hash(&collision), TARGET)
+        }
+    }
+
+    println!("{:?}", now.elapsed());
+}
+
+const fn fnv_hash(data: &[u8]) -> u32 {
+    let mut hash: u32 = 0;
+    let mut i = 0;
+    while i < data.len() {
+        hash = hash.wrapping_mul(FNV_PRIME).wrapping_add(data[i] as u32);
+        i += 1;
+    }
+    hash
+}
+
+/// Find bytes strings `m` of length at most `max_len` such that
+///
+/// ```text
+/// fnv_hash(prefix|m|suffix) == target_hash
+/// ```
+///
+/// The maximum value of `max_len` is 8.
+///
+/// The search is optimized by using iterative DFS to avoid recomputing
+/// hashes, mathematically solving for the possible value of the last
+/// character and parallelizing the above over second-to-last characters
+/// using `L`-lane SIMD.
 fn find_collisions_simd<const L: usize>(
     prefix: &[u8],
     suffix: &[u8],
@@ -113,14 +138,11 @@ where
     LaneCount<L>: SupportedLaneCount,
     Simd<u32, L>: SimdPartialEq<Mask = Mask<i32, L>>,
 {
-    let mut matches = Vec::with_capacity(8);
-
     let suffix = PrecomputedSuffix::new(suffix, target_hash);
     let prefix_hash = fnv_hash(prefix);
+    let mut matches = Vec::with_capacity(8);
 
-    println!("suffix: {suffix:x?}");
-
-    // Manually check plain prefix (len 0)
+    // check the empty string (matches if prefix|suffix matches)
     if prefix_hash == target_hash {
         matches.push(Match {
             bytes_be: 0,
@@ -128,8 +150,9 @@ where
         })
     }
 
-    // Manually check first character (len 1)
-    let one_length_collision = suffix.target_shift.wrapping_sub(prefix_hash);
+    // check one-character strings by directly solving for the possible value
+    let prefix_hash_base = prefix_hash.wrapping_mul(FNV_PRIME);
+    let one_length_collision = suffix.target_shift.wrapping_sub(prefix_hash_base);
     if ALPHABET.contains(one_length_collision) {
         matches.push(Match {
             bytes_be: one_length_collision as u64,
@@ -137,12 +160,13 @@ where
         })
     }
 
-    // having 2 vecs means that we can copy the hash vectors straight into the queue
+    // having 2 vecs means that we can copy the next_hash_base vectors straight into
+    // the DFS stack
     let init_cap = max_len * ALPHABET.bytes().len();
     let mut hash_base_stack = Vec::with_capacity(init_cap);
     let mut match_stack = Vec::with_capacity(init_cap);
 
-    hash_base_stack.push(prefix_hash.wrapping_mul(FNV_PRIME));
+    hash_base_stack.push(prefix_hash_base);
     match_stack.push(Match {
         bytes_be: 0,
         len: 2,
@@ -153,12 +177,17 @@ where
     while let (Some(hash_base), Some(seq)) = (hash_base_stack.pop(), match_stack.pop()) {
         let hash_base_splat = Simd::splat(hash_base);
 
-        let (alphabet_chunks, alphabet_remainder) = const { ALPHABET.simd_groups::<L>() };
+        // use simd to process second-to-last characters in parallel
+        //
+        // because these chunks are known at compile-time the loops below can be unrolled
+        // and bounds checks can be removed
+        let (alphabet_chunks, alphabet_remainder) = const { ALPHABET.simd_chunks::<L>() };
 
         for chunk in alphabet_chunks.as_slice() {
             let next_hash_base = (hash_base_splat + chunk) * Simd::splat(FNV_PRIME);
             let chunk_arr = chunk.as_array();
 
+            // add len+1 strings to the DFS stack
             if seq.len != max_len {
                 hash_base_stack.extend_from_slice(next_hash_base.as_array());
                 match_stack.extend(chunk_arr.iter().map(|&c| Match {
@@ -166,7 +195,7 @@ where
                     len: seq.len + 1,
                 }));
             }
-
+            // solve for the only last character that could collide and report matches
             let solutions = target_shift_splat - next_hash_base;
             if unlikely(ALPHABET.simd_prefilter(solutions)) {
                 matches.extend(
@@ -185,6 +214,7 @@ where
         for &c in alphabet_remainder.as_slice() {
             let next_hash_base = (hash_base + c).wrapping_mul(FNV_PRIME);
 
+            // add len+1 strings to the DFS stack
             if seq.len != max_len {
                 hash_base_stack.push(next_hash_base);
                 match_stack.push(Match {
@@ -192,7 +222,7 @@ where
                     len: seq.len + 1,
                 });
             }
-
+            // solve for the only last character that could collide and report matches
             let s = suffix.target_shift - next_hash_base;
             if unlikely(ALPHABET.contains(s)) {
                 matches.push(Match {
@@ -205,63 +235,3 @@ where
 
     matches
 }
-
-// fn blast_all() {
-//     for &c in START {
-//         let mut prefix = PREFIX.to_owned();
-//         prefix.push(c);
-//         blast(&prefix, SUFFIX);
-//     }
-// }
-
-// fn blast(prefix: &[u8], suffix: &[u8]) {
-//     let mut base_hash: u32 = 0;
-//     for c in prefix {
-//         base_hash = base_hash.wrapping_mul(37).wrapping_add(*c as u32);
-//     }
-
-//     let max = ALPHABET.len().pow(SEARCH as u32);
-//     let par = (0..max)
-//         .into_par_iter()
-//         .filter(|i| test_hash(base_hash, suffix, *i));
-
-//     let result: Vec<_> = par.collect();
-
-//     for i in result {
-//         print_path(prefix, suffix, i);
-//     }
-// }
-
-// fn test_hash(base_hash: u32, suffix: &[u8], i: usize) -> bool {
-//     let mut hash: u32 = base_hash;
-//     let mut rem = i;
-//     loop {
-//         hash = hash
-//             .wrapping_mul(37)
-//             .wrapping_add(ALPHABET[rem % ALPHABET.len()] as u32);
-//         rem /= ALPHABET.len();
-//         if rem == 0 {
-//             break;
-//         }
-//     }
-
-//     for c in suffix {
-//         hash = hash.wrapping_mul(37).wrapping_add(*c as u32);
-//     }
-
-//     hash == TARGET
-// }
-
-// fn print_path(prefix: &[u8], suffix: &[u8], i: usize) {
-//     let mut output = prefix.to_owned();
-//     let mut rem = i;
-//     loop {
-//         output.push(ALPHABET[rem % ALPHABET.len()]);
-//         rem /= ALPHABET.len();
-//         if rem == 0 {
-//             break;
-//         }
-//     }
-//     output.extend_from_slice(suffix);
-//     println!("{}", String::from_utf8_lossy(&output));
-// }
