683228ecec5670c965d4d41c00348ea8ad15146e
William Tremblay <tremwil@gmail.com>
2026-01-19 13:24:25 +0000

    add opencl version

---
+++ opencl/src/kernel.cl
@@ -0,0 +1,106 @@
+#ifndef PAR_LEN
+#define PAR_LEN 4 // number of chars the work is split over
+#endif
+#ifndef SEQ_LEN
+#define SEQ_LEN 4 // number of chars to check sequentially in a work item
+#endif
+#ifndef HASH_T
+#define HASH_T uint // hash integer type
+#endif
+
+#define SEARCH_DEPTH (SEQ_LEN - 1)
+#define FNV_PRIME 37
+
+typedef HASH_T hash_t;
+
+const uchar ALPHABET[] = ".0123456789_abcdefghijklmnopqrstuvwxyz";
+const size_t ALPHABET_SIZE = sizeof(ALPHABET) - 1;
+
+bool in_alphabet(hash_t solution) {
+    if (solution > 'z') return false;
+    if (solution < '.') return false;
+    if (solution >= 'a') return true;
+    if (solution > '_') return false;
+    if (solution == '_') return true;
+    if (solution > '9') return false;
+    return solution != '/'; // only char between . and 0
+}
+
+typedef struct {
+    uchar bytes[PAR_LEN];
+} ItemBase;
+
+typedef struct {
+    ItemBase base;
+    uchar bytes[SEQ_LEN];
+} Match;
+
+kernel void find_collisions(
+    const uint work_items,
+    const hash_t prefix_hash,
+    const hash_t suffix_shift,
+    global Match* out_buffer,
+    const uint out_buffer_size,
+    volatile global int* out_buffer_written
+) {
+    // get global item index (encodes item-specific static prefix)
+    const size_t item_index = get_global_id(0);
+    if (item_index >= work_items) {
+        return;
+    }
+
+    // compute item-specific base hash and bytes for work item
+    hash_t item_base_hash = prefix_hash * FNV_PRIME;
+    uint encoded = item_index;
+    ItemBase item_base;
+    
+    for (int i = 0; i < PAR_LEN; i++) {
+        uchar chr = ALPHABET[encoded % ALPHABET_SIZE];
+        item_base.bytes[i] = chr;
+        item_base_hash = (item_base_hash + chr) * FNV_PRIME;
+        encoded /= ALPHABET_SIZE;
+    }
+
+    // DFS state variables
+    hash_t base_hashes[SEARCH_DEPTH] = { [0] = item_base_hash };
+    char char_indices[SEARCH_DEPTH];
+    char depth = 0;
+
+    // Init char indices
+    for (int i = 0; i < SEARCH_DEPTH; i++) {
+        char_indices[i] = -1;
+    }
+
+    // DFS loop
+    while (depth >= 0) {
+        const char i = ++char_indices[depth];
+        if (i >= ALPHABET_SIZE) {
+            char_indices[depth--] = -1;
+            continue;
+        }
+
+        const hash_t base_hash = (base_hashes[depth] + (hash_t)ALPHABET[i]) * FNV_PRIME;
+        const hash_t solution = suffix_shift - base_hash;
+        if (in_alphabet(solution)) {
+            const uint slot = atomic_add(out_buffer_written, 1);
+            if (slot < out_buffer_size) {
+                global Match* m = out_buffer + slot;
+                // write base (par) bytes
+                m->base = item_base;
+                // write seq bytes
+                for (int j = 0; j <= depth; j++) {
+                    m->bytes[j] = ALPHABET[char_indices[j]];
+                }
+                m->bytes[depth+1] = solution;
+                // nul-terminate
+                if (depth + 2 < SEQ_LEN) {
+                    m->bytes[depth+2] = 0;
+                }
+            }
+        }
+
+        if (depth < SEARCH_DEPTH - 1) {
+            base_hashes[++depth] = base_hash; 
+        }
+    }
+}
---
+++ opencl/src/main.rs
@@ -0,0 +1,248 @@
+use std::{cmp::Reverse, ffi::c_void, process::exit, ptr, time::Instant};
+
+use cl3::{
+    ext::{
+        CL_BLOCKING, CL_DEVICE_AVAILABLE, CL_DEVICE_MAX_CLOCK_FREQUENCY,
+        CL_DEVICE_MAX_COMPUTE_UNITS, CL_DEVICE_VERSION, CL_MEM_READ_WRITE, CL_MEM_USE_HOST_PTR,
+        CL_MEM_WRITE_ONLY,
+    },
+    info_type::InfoType,
+};
+use opencl3::{
+    command_queue::CommandQueue,
+    context::Context,
+    device::{CL_DEVICE_NAME, CL_DEVICE_TYPE_GPU, Device, get_all_devices, get_device_info},
+    error_codes::ClError,
+    kernel::{ExecuteKernel, Kernel},
+    memory::Buffer,
+    program::Program,
+};
+
+type Hash = u32;
+
+const FNV_PRIME: Hash = 37;
+const ALPHABET: &[u8] = b".0123456789_abcdefghijklmnopqrstuvwxyz";
+
+const PREFIX: &[u8] = b"/other/";
+const SUFFIX: &[u8] = b".dcx";
+const TARGET: Hash = 0xd7255946;
+
+const PAR_LEN: usize = 4; // Assign a gpu thread to each prefix of this length
+const SEQ_LEN: usize = 4; // Search for collisions of this many extra chars
+
+const BLOCK_SIZE: usize = 512; // tune this for your GPU
+const TOTAL_LEN: usize = PAR_LEN + SEQ_LEN;
+
+fn main() -> Result<(), Err> {
+    let suffix = PrecomputedSuffix::new(SUFFIX, TARGET);
+    let prefix_hash = fnv_hash(PREFIX);
+
+    let devices = get_all_devices(CL_DEVICE_TYPE_GPU)?;
+    let mut usable: Vec<_> = devices
+        .into_iter()
+        .filter(|&dev| {
+            match get_device_info(dev, CL_DEVICE_AVAILABLE) {
+                Ok(InfoType::Uint(1..)) => (),
+                _ => return false,
+            }
+            if let Ok(InfoType::VecUchar(ver)) = get_device_info(dev, CL_DEVICE_VERSION) {
+                // for global int32 atomics support
+                return ver.as_slice() >= b"1.1";
+            }
+            false
+        })
+        .filter_map(|dev| {
+            let max_clock = get_device_info(dev, CL_DEVICE_MAX_CLOCK_FREQUENCY).ok()?;
+            let max_cu = get_device_info(dev, CL_DEVICE_MAX_COMPUTE_UNITS).ok()?;
+            match (max_clock, max_cu) {
+                (InfoType::Uint(clock), InfoType::Uint(cu)) => Some((dev, clock * cu)),
+                _ => None,
+            }
+        })
+        .collect();
+
+    usable.sort_by_key(|&(_, compute)| Reverse(compute));
+
+    if usable.is_empty() {
+        println!("no usable OpenCL GPU device found.");
+        exit(1);
+    }
+
+    println!("usable devices (OpenCL support >= 1.1):");
+    for (i, &(dev, compute)) in usable.iter().enumerate() {
+        let name = get_device_info(dev, CL_DEVICE_NAME)?;
+        println!("{i}: {name}, effective compute {compute} MHz");
+    }
+
+    println!("\nusing device 0.");
+
+    let device = Device::new(usable[0].0);
+    let context = Context::from_device(&device)?;
+    let queue = CommandQueue::create_default(&context, 0)?;
+
+    let hash_type = if size_of::<Hash>() == 4 {
+        "uint"
+    } else {
+        "ulong"
+    };
+    let program = Program::create_and_build_from_source(
+        &context,
+        include_str!("kernel.cl"),
+        &format!("-D PAR_LEN={PAR_LEN} -D SEQ_LEN={SEQ_LEN} -D HASH_T={hash_type} -Werror"),
+    )
+    .expect("kernel failed to build");
+
+    let kernel = Kernel::create(&program, "find_collisions")?;
+
+    let work_items = ALPHABET.len().pow(PAR_LEN as u32);
+    let work_size = work_items.next_multiple_of(BLOCK_SIZE);
+
+    let expected_collisions =
+        (ALPHABET.len() as f64).powi(TOTAL_LEN as i32) / 256f64.powi(size_of::<Hash>() as i32);
+    let buf_len = (1.5 * expected_collisions) as usize + 100; // safety margin
+    let buf_len_bytes = buf_len * TOTAL_LEN;
+    if buf_len_bytes > u32::MAX as usize {
+        panic!("results buffer too big")
+    }
+
+    println!("using {buf_len} element results buffer\n");
+
+    let results_dev = unsafe {
+        Buffer::<u8>::create(&context, CL_MEM_WRITE_ONLY, buf_len_bytes, ptr::null_mut())?
+    };
+    let results_count_dev = unsafe {
+        static ZERO: &u32 = &0;
+        Buffer::<u32>::create(
+            &context,
+            CL_MEM_READ_WRITE | CL_MEM_USE_HOST_PTR,
+            1,
+            ZERO as *const u32 as *mut c_void,
+        )?
+    };
+
+    let pre_kernel = Instant::now();
+
+    let kernel_event = unsafe {
+        ExecuteKernel::new(&kernel)
+            .set_arg(&(work_items as u32))
+            .set_arg(&prefix_hash)
+            .set_arg(&suffix.target_shift)
+            .set_arg(&results_dev)
+            .set_arg(&(buf_len as u32))
+            .set_arg(&results_count_dev)
+            .set_global_work_size(work_size)
+            .set_local_work_size(BLOCK_SIZE)
+            .enqueue_nd_range(&queue)?
+    };
+
+    // wait for kernel completion and read result count
+    let mut results_count = 0;
+    unsafe {
+        queue.enqueue_read_buffer(
+            &results_count_dev,
+            CL_BLOCKING,
+            0,
+            std::slice::from_mut(&mut results_count),
+            &[kernel_event.get()],
+        )?
+    };
+    results_count = results_count.min(buf_len as u32);
+    let kernel_time = pre_kernel.elapsed();
+
+    // copy initialized portion of results buffer
+    let mut results = vec![0; results_count as usize * TOTAL_LEN];
+    unsafe {
+        queue.enqueue_read_buffer(&results_dev, CL_BLOCKING, 0, results.as_mut_slice(), &[])?
+    };
+
+    // print matches
+    for res in results.chunks_exact(TOTAL_LEN) {
+        let len = res.iter().position(|&b| b == 0).unwrap_or(res.len());
+        println!(
+            "{}{}{}",
+            String::from_utf8_lossy(PREFIX),
+            String::from_utf8_lossy(&res[..len]),
+            String::from_utf8_lossy(SUFFIX)
+        );
+    }
+
+    println!("\nfound {} solutions in {:?}", results_count, kernel_time);
+
+    Ok(())
+}
+
+const fn fnv_hash(bytes: &[u8]) -> Hash {
+    let mut hash: Hash = 0;
+    let mut i = 0;
+    while i < bytes.len() {
+        hash = hash.wrapping_mul(FNV_PRIME).wrapping_add(bytes[i] as Hash);
+        i += 1;
+    }
+    hash
+}
+
+/// Precomputed information about the hash of a suffix.
+///
+/// Used to efficiently compute the combined hash of `base|suffix` given `hash(base)`
+/// as well as efficiently finding a single character `x` such that
+/// `hash(base|x|suffix) == target_hash`.
+#[derive(Debug, Clone, Copy)]
+#[allow(unused)]
+struct PrecomputedSuffix {
+    hash: Hash,
+    mult: Hash,
+    target_shift: Hash,
+}
+
+impl PrecomputedSuffix {
+    pub const fn new(suffix: &[u8], target_hash: Hash) -> Self {
+        // 64-bit modular inverse using 4 Newton-Raphson iterations
+        // From https://arxiv.org/abs/2204.04342
+        const fn minv32(a: Hash) -> Hash {
+            assert!(!a.is_multiple_of(2));
+
+            let mut x = 3u32.wrapping_mul(a) ^ 2;
+            let mut y = 1u32.wrapping_sub(a.wrapping_mul(x));
+
+            x = x.wrapping_mul(y.wrapping_add(1));
+            y = y.wrapping_mul(y);
+            x = x.wrapping_mul(y.wrapping_add(1));
+            y = y.wrapping_mul(y);
+            x = x.wrapping_mul(y.wrapping_add(1));
+            y = y.wrapping_mul(y);
+
+            x.wrapping_mul(y.wrapping_add(1))
+        }
+
+        let hash = fnv_hash(suffix);
+        let mult = FNV_PRIME.wrapping_pow(suffix.len() as Hash);
+        let target_shift = target_hash.wrapping_sub(hash).wrapping_mul(minv32(mult));
+
+        Self {
+            hash,
+            mult,
+            target_shift,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Err(#[allow(unused)] ClError);
+
+impl std::fmt::Display for Err {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        std::fmt::Display::fmt(&self, f)
+    }
+}
+
+impl From<ClError> for Err {
+    fn from(value: ClError) -> Self {
+        Self(value)
+    }
+}
+
+impl From<i32> for Err {
+    fn from(value: i32) -> Self {
+        Self(ClError(value))
+    }
+}
